a8c0b269cef99c1fc03dd4a8ac6f15e447b9d62e
JungGeunt <jktrhc@naver.com>
2024-01-22 11:47:39 +0900

    first commit

---
+++ src/main/java/com/example/codepipeline/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.codepipeline;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/sayHello/{name}")
+    public String sayHello(@PathVariable String name){
+        return  "Hello" +name;
+    }
+
+    @GetMapping("/}")
+    public String root(){
+        return  "HI This Page Before Modifiyed";
+    }
+}
